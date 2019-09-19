1cd5ed201b6a28de3a6100ca28e2ed870b5f4c3b
Krisli Dimo <krislidimo@gmail.com>
2019-09-19 15:02:01 -0500

    added comments & day 2 exercises.

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -45,16 +45,54 @@ class BinarySearchTree:
     else:
       return self.getMaxHelper(target)
 
+  # Insert the given value into the tree
   def insert(self, value):
     return self.insertHelper(value, self.root)
 
+  # Return True if the tree contains the value
+  # False if it does not
   def contains(self, value):
     return self.containsHelper(value, self.root)
 
+  # Return the maximum value found in the tree
   def get_max(self):
     return self.getMaxHelper(self.root)
 
+  # Call the function `cb` on the value of each node
+  # You may use a recursive or iterative approach
   def for_each(self, cb):
+    cb(self.value)
+    if self.left:
+      self.left.for_each(cb)
+    if self.right:
+      self.right.for_each(cb)
+
+ # DAY 2 Project -----------------------
+
+  # Print all the values in order from low to high
+  # Hint:  Use a recursive, depth first traversal
+  def in_order_print(self, node):
+    pass
+
+  # Print the value of every node, starting with the given node,
+  # in an iterative breadth first traversal
+  def bft_print(self, node):
+    pass
+
+  # Print the value of every node, starting with the given node,
+  # in an iterative depth first traversal
+  def dft_print(self, node):
+    pass
+
+  # STRETCH Goals -------------------------
+  # Note: Research may be required
+
+  # Print In-order recursive DFT
+  def pre_order_dft(self, node):
+    pass
+
+  # Print Post-order recursive DFT
+  def post_order_dft(self, node):
     pass
 
 # bst = BinarySearchTree(5)
